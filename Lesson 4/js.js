07bb3b6f37759a5c3cedc9cc71cd0b7b5482ff79
Kenny Castillo <59597501+kcastil11@users.noreply.github.com>
2020-02-01 22:45:53 -0800

    Delete js.js

--- Lesson 4/js.js
+++
@@ -1,2 +0,0 @@
-const options={hour: 'numeric', minute: 'numeric',weekday:'long', day: 'numeric', month: 'long', year:'numeric'};
-document.getElementById('upDate').textContent= new Date().toLocaleDateString('en-US', options);
