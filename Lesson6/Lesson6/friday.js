10ea23db0ff1d50c6279869f1219a61a2ccba96f
Kenny Castillo <59597501+kcastil11@users.noreply.github.com>
2020-02-17 02:39:56 -0800

    Delete friday.js

--- Lesson6/Lesson6/friday.js
+++
@@ -1,7 +0,0 @@
-var today = new Date();
-
-if (today.getDay() == 5 ) {
-  document.querySelector("aside").style.display = "block";
-} else {
-  document.querySelector("aside").style.display = "none";
-}
