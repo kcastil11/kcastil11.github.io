1a8aa728fe06cf0e34790e97f69c7e9a2eef93f9
Kenny Castillo <59597501+kcastil11@users.noreply.github.com>
2020-02-17 02:34:42 -0800

    Add files via upload

---
+++ Lesson6/Lesson6/windchill.js
@@ -0,0 +1,16 @@
+function windChill() {
+    let windSpeed = document.getElementById('windSp').innerHTML;
+    windSpeed = parseFloat(windSpeed);
+    let temperature = document.getElementById('temp').innerHTML;
+    temperature = parseFloat(temperature);
+    if (temperature <= 50 && windSpeed >= 3) {
+        let windspeedExponent = Math.pow(windSpeed, 0.16);
+        let windChill = (35.74 + 0.6215 * temperature - 35.75 * windspeedExponent + 0.4275 * temperature * windspeedExponent).toFixed(0);
+        return windChill;
+    } else {
+        return temperature;
+    }
+
+}
+// Wind Chill calculation
+document.getElementById("displayWind").innerHTML = windChill() + "&#xb0;F";
